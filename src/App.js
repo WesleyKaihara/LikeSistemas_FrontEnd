4341d63eafabd574e8b6b9ec46579c64fcd36927
wesleyKaihara <wesleykaihara@gmail.com>
2022-05-14 18:44:36 -0300

    Commit Inicial - Configuração proxy e database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+
 function App() {
+
+  const [serverData, setServerData] = useState();
+
+  useEffect(() => {
+    fetch("/subCategorias")
+      .then(
+        res => res.json()
+      ).then(
+        data => {
+          setServerData(data);
+        }
+      )
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {((typeof serverData === 'undefined') ? (
+        <p>Loading ...</p>
+      ) : (
+        serverData.response.map((item) => (
+          <p key={item.ID}>{item.NOME}</p>
+        ))
+      )
+      )}
     </div>
   );
 }
